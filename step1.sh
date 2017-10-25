b45341889e1d1d37a3e576b34566076dc8d2c371
techmore <sdolbec1@gmail.com>
2017-10-25 15:34:26 -0400

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -11,15 +11,15 @@ ethernet=`ls  /sys/class/net | grep -v lo`
 
 # Setup static IP and local DNS
 
-cat <<EOF_interfaces >> /etc/network/interfaces 
-auto $ethernet
-iface $ethernet inet static
-   address 10.0.0.10
-   netmask 255.255.255.0
-   gateway 10.0.0.1
-   dns-nameservers 127.0.0.1 8.8.8.8
-   dns-search 
-EOF_interfaces
+#cat <<EOF_interfaces >> /etc/network/interfaces 
+#auto $ethernet
+#iface $ethernet inet static
+#   address 10.0.0.10
+#   netmask 255.255.255.0
+#   gateway 10.0.0.1
+#   dns-nameservers 127.0.0.1 8.8.8.8
+#   dns-search 
+# EOF_interfaces
 
 apt-get update -y; apt-get upgrade -y; apt-get dist-upgrade -y
 hostnamectl set-hostname $newHostname
